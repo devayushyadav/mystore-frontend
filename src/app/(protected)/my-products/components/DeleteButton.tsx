a2accdfbe3dd621e8c3ab04fb669be808bc7cbe6
Ayush Yadav <ayushyadav@luminoguru.com>
2024-09-23 17:05:36 +0530

    Product Delete API integrated

--- src/app/(protected)/my-products/components/DeleteButton.tsx
+++ src/app/(protected)/my-products/components/DeleteButton.tsx
@@ -1,6 +1,11 @@
-'use client';
-import React from 'react';
-import styles from '../ProductList.module.css';
+"use client";
+import React, { useState } from "react";
+import styles from "../ProductList.module.css";
+import axiosInstance from "@/services/axios";
+import toast from "react-hot-toast";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import { useCookies } from "next-client-cookies";
 
 // Define prop types using TypeScript
 interface DeleteProps {
@@ -8,13 +13,53 @@ interface DeleteProps {
 }
 
 const Delete: React.FC<DeleteProps> = ({ productId }) => {
+  const cookies = useCookies();
+
+  const token = cookies.get("token");
+  const [loading, _loading] = useState<boolean>(false);
+
   const handleDelete = (id: string) => {
-    // Placeholder function for deleting a product
-    console.log(`Delete product with id: ${id}`);
+    if (loading) {
+      return;
+    }
+    confirmAlert({
+      title: "Confirm to delete",
+      message: "Are you sure to do this?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => {
+            axiosInstance
+              .delete(`/my-products/${id}`, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              })
+              .then((resp) => {
+                toast.success(resp.data.message);
+                if (resp.data.success) {
+                }
+                _loading(false);
+              })
+              .catch((err) => {
+                const resp = err.response;
+                toast.error(resp.data.message);
+                _loading(false);
+              });
+          },
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
   };
 
   return (
-    <button onClick={() => handleDelete(productId)} className={styles.iconButton}>
+    <button
+      onClick={() => handleDelete(productId)}
+      className={styles.iconButton}
+    >
       {/* Delete Icon */}
       <svg className={styles.icon} viewBox="0 0 24 24">
         <path d="M16 9v10H8V9h8m-1.5-6H9.5l-1 1H5v2h14V4h-3.5l-1-1z" />
@@ -23,4 +68,4 @@ const Delete: React.FC<DeleteProps> = ({ productId }) => {
   );
 };
 
-export default Delete
+export default Delete;
